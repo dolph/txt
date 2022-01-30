d667d963949dcb72a38032173e0e8480fbec4c8a
Dolph Mathews <dolph.mathews@gmail.com>
2022-01-30 13:42:47 -0600

    pass CLI args as message

--- src/txt.go
+++ src/txt.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+)
 
 func main() {
-	fmt.Println("hello world")
+	client := twilio.NewRestClient()
+	from := os.Getenv("TXT_FROM")
+	to := os.Getenv("TXT_TO")
+
+	params := &openapi.CreateMessageParams{}
+	params.SetTo(to)
+	params.SetFrom(from)
+	params.SetBody(strings.Join(os.Args[1:], " "))
+
+	resp, err := client.ApiV2010.CreateMessage(params)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		response, _ := json.Marshal(*resp)
+		fmt.Println("Response: " + string(response))
+	}
 }
